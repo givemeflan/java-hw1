42f8af7e0674c7958acfb68454ca1a416cbf9444
Laura Le <lle387@gmail.com>
2020-11-10 15:32:56 -0500

    adding variables

---
+++ hw.java
@@ -0,0 +1,19 @@
+
+public class hw {
+    // Running First Function Here
+    public static void main(String[] args) {
+
+        // In this scenario, I'm setting variables to my age and my favorite color. This
+        // would work in JavaScript but not in Java. This syntax may break the results.
+
+        myAge = 33;
+        myColor = "pink";
+
+        // With Java, we have to be specific with our variables. I'm setting the proper
+        // variable type here.
+
+        int age = 33;
+        String favColor = "pink";
+    }
+
+    
