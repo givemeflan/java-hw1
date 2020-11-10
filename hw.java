4725223b70fbcf9aa1beb75551d830e68ca4daae
Laura Le <lle387@gmail.com>
2020-11-10 16:03:08 -0500

    adding comment to file

--- hw.java
+++ hw.java
@@ -1,4 +1,6 @@
 
+import java.until.scanner;
+
 public class hw {
     // Running First Function Here
     public static void main(String[] args) {
@@ -12,8 +14,9 @@ public class hw {
         // With Java, we have to be specific with our variables. I'm setting the proper
         // variable type here.
 
-        int age = 33;
-        String favColor = "pink";
+        int myAge = 33;
+        String myColor = "pink";
     }
 
-    
+// I just want to say, I'm not really sure what the questions are asking from
+// me. If I fail this hw assignment, I'm sorry lol. The questions are confusing.
